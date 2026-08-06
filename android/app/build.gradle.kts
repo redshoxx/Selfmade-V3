50ce4f3932479df42b3a23a2abe7e1da78b3f490
redshoxx <48377562+redshoxx@users.noreply.github.com>
2026-08-06 15:22:18 +0200

    Bump Android wrapper to V20

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "at.selfmade.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19300
-        versionName = "19.3.0"
+        versionCode = 20000
+        versionName = "20.0.0"
     }
 
     buildTypes {
