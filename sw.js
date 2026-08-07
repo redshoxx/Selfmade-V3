849aa16642cdbfd40c2a8fcbe6cccb5d2b21b9a6
redshoxx <48377562+redshoxx@users.noreply.github.com>
2026-08-07 17:27:56 +0200

    V3.1: PWA-Cache erneuern

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
-const CACHE = 'selfmade-v3.0.0'
+const CACHE = 'selfmade-einkauf-v3.1.0'
 const SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg']
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())))
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())))
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()))
+})
+
 self.addEventListener('fetch', event => {
-  if(event.request.method!=='GET') return
-  const url=new URL(event.request.url); if(url.origin!==self.location.origin) return
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{if(response.ok)caches.open(CACHE).then(cache=>cache.put('/index.html',response.clone()));return response}).catch(()=>caches.match('/index.html')))
+  if (event.request.method !== 'GET') return
+  const url = new URL(event.request.url)
+  if (url.origin !== self.location.origin) return
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request, { cache: 'no-store' }).then(response => {
+      if (response.ok) caches.open(CACHE).then(cache => cache.put('/index.html', response.clone()))
+      return response
+    }).catch(() => caches.match('/index.html')))
     return
   }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok)caches.open(CACHE).then(cache=>cache.put(event.request,response.clone()));return response})))
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()))
+    return response
+  })))
 })
